a73aff0bdd3b8d23122b1d20e5a42cf9fce02f26
Nathan Lui <nathanlui2010@gmail.com>
2019-11-16 15:46:04 -0800

    demo

--- nathan.py
+++ nathan.py
@@ -1,2 +1,14 @@
+
+
+
+while True:
+    key = input("(L)eft, (R)ight, (F)orward? ")
+    if key.lower()[0] is "l":
+        print("You moved left")
+    if key.lower()[0] is "r":
+        print("You moved right")
+    if key.lower()[0] is "f":
+        print("You moved forward")
+
 print("Hello World")
 
